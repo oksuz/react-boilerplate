bca0aa1badd1606d90139076eb4b24408b2abf42
Yunus Oksuz <yunusoksuz@gmail.com>
2020-02-23 22:33:25 +0300

    indent

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    devtool: 'source-map',
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: "index_bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: '/node_modules',
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-        }),
-    ],
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: "/node_modules",
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 };
